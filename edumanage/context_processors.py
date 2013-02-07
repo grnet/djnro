588cd3eb0916f7fe9bbcd5b7e3e720c995c6eac3
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2013-02-07 12:11:25 +0200

    Updated NRO context processor

--- edumanage/context_processors.py
+++ edumanage/context_processors.py
@@ -2,4 +2,12 @@ from django.conf import settings
 
 def country_code(context):
     # return the value you want as a dictionnary. you may add multiple values in there.
-    return {'COUNTRY_CODE': settings.COUNTRY_CODE, 'DOMAIN_MAIN_URL': settings.DOMAIN_MAIN_URL, 'DOMAIN_HELPDESK_DICT': settings.DOMAIN_HELPDESK_DICT}
+    return {
+            'COUNTRY_NAME': settings.NRO_COUNTRY_NAME,
+            'COUNTRY_CODE': settings.NRO_COUNTRY_CODE, 
+            'DOMAIN_MAIN_URL': settings.NRO_DOMAIN_MAIN_URL, 
+            'DOMAIN_HELPDESK_DICT': settings.NRO_DOMAIN_HELPDESK_DICT,
+            'MAP_CENTER': settings.MAP_CENTER,
+            'DEV_TEAM': settings.NRO_DEV_BY_DICT,
+            'SOCIAL_MEDIA_LIST': settings.NRO_DEV_SOCIAL_MEDIA_CONTACT,
+            }
