b7c658c7fa6b5ea031aa5853e1f1b8b4f7c08bee
Leonidas Poulopoulos <leopoul@noc.grnet.gr>
2014-05-13 11:40:17 +0300

    Add mgmt command to check active CAT enrollemtns

---
+++ edumanage/management/commands/check_cat_active.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- vim:encoding=utf-8:
+# vim: tabstop=4:shiftwidth=4:softtabstop=4:expandtab
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
+warnings.simplefilter("ignore", FutureWarning)
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from utils.cat_helper import *
+from edumanage.models import CatEnrollment 
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Checks if CAT enrollments have active registered IdP admins'
+
+    def handle(self, *args, **options):
+        all_cat_entries = CatEnrollment.objects.all()
+        for catentry in all_cat_entries:
+            instance = catentry.cat_instance
+            cat_instance = settings.CAT_AUTH[instance]
+            cat_api_key = cat_instance['CAT_API_KEY']
+            cat_api_url = cat_instance['CAT_API_URL']
+            inst_uid = catentry.cat_inst_id
+            params = {"INST_IDENTIFIER":inst_uid}
+            c = CatQuery(cat_api_key, cat_api_url)
+            cq = c.admincount(params)
+            if cq:
+                if c.response['number_of_admins'] > 0:
+                    if catentry.url != "ACTIVE":
+                        catentry.url = "ACTIVE"
+                        catentry.save()
