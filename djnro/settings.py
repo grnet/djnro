734b3220cbf39fcd60d6765430b5a8b2a6e072fb
Kroustouris Stauros <staurosk@noc.grnet.gr>
2014-10-01 17:55:17 +0300

    move templates in the root of the project

--- djnro/settings.py
+++ djnro/settings.py
@@ -146,7 +146,7 @@ ROOT_URLCONF = 'djnro.urls'
 WSGI_APPLICATION = 'djnro.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(djnro_path, 'templates/'),
+    os.path.join(project_path, 'templates/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
