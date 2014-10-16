48bf58c98d3f1488238837907a3fad814c931328
Simon Gray <simon.gray@reannz.co.nz>
2014-10-16 16:00:01 +1300

    added in all of the non-specific to REANNZ changes

--- djnro/urls.py
+++ djnro/urls.py
@@ -18,9 +18,9 @@ urlpatterns = patterns('',
     url(r'^logout/?', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="logout"),
     url(r'^registration/accounts/activate/(?P<activation_key>\w+)/$', 'accounts.views.activate', name='activate_account'),
     url(r'^registration/activate/complete/$',
-                           direct_to_template,
-                           { 'template': 'registration/activation_complete.html' },
-                           name='registration_activation_complete'),
+        direct_to_template,
+        { 'template': 'registration/activation_complete.html' },
+        name='registration_activation_complete'),
     (r'^tinymce/', include('tinymce.urls')),
 )
 
