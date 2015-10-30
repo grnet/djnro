d987563ce1c55c5c977308d947179820ff9497ee
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2015-10-30 17:46:23 +0200

    Add missing copyright header in fetch_kml

--- edumanage/management/commands/fetch_kml.py
+++ edumanage/management/commands/fetch_kml.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*- vim:encoding=utf-8:
 # vim: tabstop=4:shiftwidth=4:softtabstop=4:expandtab
+
+# Copyright © 2011-2015 Greek Research and Technology Network (GRNET S.A.)
+#
+# Developed by Leonidas Poulopoulos (leopoul-at-noc-dot-grnet-dot-gr),
+# Zenon Mousmoulas (zmousm-at-noc-dot-grnet-dot-gr) and Stavros Kroustouris
+# (staurosk-at-noc-dot-grnet-dot-gr), GRNET NOC
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHORS DISCLAIM ALL WARRANTIES WITH REGARD
+# TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+# DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+# SOFTWARE.
 import warnings
 warnings.simplefilter("ignore", DeprecationWarning)
 from django.core.management.base import BaseCommand
