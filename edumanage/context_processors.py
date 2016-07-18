9619e16345afd50bdd6a422f85bbbd5e1f87bc3c
Chris Rohrer <chris.rohrer@ubuntunet.net>
2016-07-18 14:23:12 +0200

    Add Google API key value to javascript scripts. Remove sensor option, as it is not needed anymore. For additional info, see https://developers.google.com/maps/documentation/javascript/get-api-key#key

--- edumanage/context_processors.py
+++ edumanage/context_processors.py
@@ -14,7 +14,8 @@ def country_code(context):
         'MAP_CENTER': settings.MAP_CENTER,
         'PROV_TEAM': settings.NRO_PROV_BY_DICT,
         'SOCIAL_MEDIA_LIST': settings.NRO_PROV_SOCIAL_MEDIA_CONTACT,
-        'VERSION': settings.SW_VERSION
+        'VERSION': settings.SW_VERSION,
+        'API_KEY': settings.GOOGLE_API_KEY
     }
 
 
