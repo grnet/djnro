grnet/djnro

commit f7e106c65712edb1c84537d983bbb23909128bce

Branding: replace use of "GRNET" with Realm name
Use settings.NRO_COUNTRY_CODE as key into settings.REALM_COUNTRIES to get country name.

Use settings.NRO_COUNTRY_CODE as key into Realm model instances to get NRO institution name.

Suggested by Zenon in discussion on #17, commited by Vlad Mencl on behalf of Zenon.

edumanage/context_processors.py
@@ -1,11 +1,13 @@
 from django.conf import settings
+from edumanage.models import Realm
 
 
 def country_code(context):
     # return the value you want as a dictionnary. you may add multiple values in there.
     return {
-        'COUNTRY_NAME': settings.NRO_COUNTRY_NAME,
         'COUNTRY_CODE': settings.NRO_COUNTRY_CODE,
+        'COUNTRY_NAME': dict(settings.REALM_COUNTRIES)[settings.NRO_COUNTRY_CODE],
+        'NRO_REALM': Realm.objects.get(country=settings.NRO_COUNTRY_CODE),
         'DOMAIN_MAIN_URL': settings.NRO_DOMAIN_MAIN_URL,
         'FEDERATION_NAME': settings.NRO_FEDERATION_NAME,
         'DOMAIN_HELPDESK_DICT': settings.NRO_DOMAIN_HELPDESK_DICT,

djnro/local_settings.py.dist
@@ -113,9 +113,7 @@ TIME_ZONE = 'Europe/Athens'
 # map center (lat, lng)
 MAP_CENTER = (36.97, 23.71)
 
-# Frontend country specific vars, eg. Greece
-NRO_COUNTRY_NAME = _('My Country')
-# Variable used by context_processor to display the "eduroam | <country_code>" in base.html
+# Variable used to determine the active Realm object (in views and context processor)
 NRO_COUNTRY_CODE = 'tld'
 # main domain url used in right top icon, eg. http://www.grnet.gr
 NRO_DOMAIN_MAIN_URL = "http://www.example.com"

edumanage/models.py
@@ -535,6 +535,17 @@ class Realm(models.Model):
             'country': self.country,
         }
 
+    def get_name(self, lang=None):
+        name = ', '.join([i.name for i in self.org_name.all()])
+        if not lang:
+            return name
+        else:
+            try:
+                name = self.org_name.get(lang=lang)
+                return name
+            except Exception:
+                return name
+
 
 # TODO: this represents a *database view* "realm_data", find a better way to write it
 class RealmData(models.Model):

edumanage/views.py
@@ -1624,7 +1624,9 @@ def user_login(request):
                     " Administrators have been notified and will activate"
                     " this account within the next days. <br>If this account"
                     " has remained inactive for a long time contact your "
-                    "technical coordinator or GRNET Helpdesk") % user.username
+                    "technical coordinator or %s Helpdesk") % (user.username,
+                                                               get_nro_name(request.LANGUAGE_CODE))
+
                 return render_to_response(
                     'status.html',
                     {'status': status, 'inactive': True},
@@ -1724,7 +1726,8 @@ def selectinst(request):
                 " Administrators have been notified and will activate "
                 "this account within the next days. <br>If this account"
                 " has remained inactive for a long time contact your technical"
-                " coordinator or GRNET Helpdesk") % userprofile.user.username
+                " coordinator or %s Helpdesk") % (userprofile.user.username,
+                                                  get_nro_name(request.LANGUAGE_CODE))
             return render_to_response(
                 'status.html',
                 {'status': error, 'inactive': True},
@@ -2343,3 +2346,8 @@ def lookupShibAttr(attrmap, requestMeta):
             if len(requestMeta[attr]) > 0:
                 return requestMeta[attr]
     return ''
+
+def get_nro_name(lang):
+    return Realm.objects.\
+        get(country=settings.NRO_COUNTRY_CODE).\
+        get_name(lang=lang)

commit ff3751f84d6ed5aba838390fd84b7aa4c21c6b2b

edumanage/context_processors: remove NRO_REALM
Remove the NRO_REALM context parameter definition in edumanage/context_processors.py

This got introduced partly accidentally in f7e106c as a result of discussion on #17.

However, this causes all page loads to break with a DoesNotExist exception if
the Realm object does not exist yet.

As the NRO_REALM context parameter is not used yet, the safest approach is to
remove it.

edumanage/context_processors.py
@@ -7,7 +7,6 @@ def country_code(context):
     return {
         'COUNTRY_CODE': settings.NRO_COUNTRY_CODE,
         'COUNTRY_NAME': dict(settings.REALM_COUNTRIES)[settings.NRO_COUNTRY_CODE],
-        'NRO_REALM': Realm.objects.get(country=settings.NRO_COUNTRY_CODE),
         'DOMAIN_MAIN_URL': settings.NRO_DOMAIN_MAIN_URL,
         'FEDERATION_NAME': settings.NRO_FEDERATION_NAME,
         'DOMAIN_HELPDESK_DICT': settings.NRO_DOMAIN_HELPDESK_DICT,