d5f37ca8929877b0054cc841aaa1324e46edfc6c
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2016-09-07 22:05:01 +0300

    Fix branding
    - Merge relevant keys in a BRANDING (sub)dict
    - Don't rename the context processor more appropriately *yet*
    - Allow styling similar to MANAGE_LOGIN_METHODS (local_image, image_url
      or fa_style, but keep the older option icon for backwards compatibility)

--- edumanage/context_processors.py
+++ edumanage/context_processors.py
@@ -7,10 +7,12 @@ def country_code(context):
         'COUNTRY_NAME': settings.NRO_COUNTRY_NAME,
         'COUNTRY_CODE': settings.NRO_COUNTRY_CODE,
         'DOMAIN_MAIN_URL': settings.NRO_DOMAIN_MAIN_URL,
-        'DOMAIN_HELPDESK_DICT': settings.NRO_DOMAIN_HELPDESK_DICT,
+        'BRANDING': {
+            'helpdesk': settings.NRO_DOMAIN_HELPDESK_DICT,
+            'social_media': settings.NRO_PROV_SOCIAL_MEDIA_CONTACT,
+            'service_provider': settings.NRO_PROV_BY_DICT,
+        },
         'MAP_CENTER': settings.MAP_CENTER,
-        'PROV_TEAM': settings.NRO_PROV_BY_DICT,
-        'SOCIAL_MEDIA_LIST': settings.NRO_PROV_SOCIAL_MEDIA_CONTACT,
         'VERSION': settings.SW_VERSION
     }
 
