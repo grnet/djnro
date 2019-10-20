28e615ebdabb2afbe2b1f8b3af6d220a39b716fb
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2019-10-20 23:30:13 +0300

    Clean up and realign MultiSelectField, MultiSelectFormField
    * Fix get_prep_value() and to_python() to return proper string, list
      values respectively
    * Remove get_db_prep_value(), as overriding it is unnecessary
    * Have contribute_to_class() provide a function adapted from
      django.db.models.Model._get_FIELD_display() and mimic the parent for
      attaching it to the model using curry()
    * Have validate() call the overridden method for each value
    * Have formfield() only override the form class with our own
    * Let MultiSelectFormField only override the form widget and remove
      extra args by means of introspection
      * extra args for TypedChoiceField added by calling parent
        models.Field.formfield()
    * Remove redundant overridden method value_to_string()

--- edumanage/models.py
+++ edumanage/models.py
@@ -2,17 +2,24 @@
 # vim: tabstop=4:shiftwidth=4:softtabstop=4:expandtab
 from collections import namedtuple
 import uuid
+try:
+    from inspect import getfullargspec as getargspec
+except ImportError:
+    from inspect import getargspec
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import fields
+from django.utils.functional import curry
 from django.utils.text import capfirst
 from django.utils import six
 from django.core import exceptions
 from django.conf import settings
 from django import forms
 from django.core.exceptions import ValidationError
-from django.utils.encoding import python_2_unicode_compatible
+from django.utils.encoding import (
+    force_text, python_2_unicode_compatible
+)
 
 
 def validate_venue_info(value):
@@ -33,84 +40,79 @@ class MultiSelectFormField(forms.MultipleChoiceField):
     widget = forms.CheckboxSelectMultiple
 
     def __init__(self, *args, **kwargs):
-        self.max_choices = 4
-        super(MultiSelectFormField, self).__init__(*args, **kwargs)
-
-    def clean(self, value):
-        if not value and self.required:
-            raise forms.ValidationError(self.error_messages['required'])
-        # if value and self.max_choices and len(value) > self.max_choices:
-        #     raise forms.ValidationError('You must select a maximum of %s choice%s.'
-        #             % (apnumber(self.max_choices), pluralize(self.max_choices)))
-        return value
-
+        supercls = super(MultiSelectFormField, self)
+        # remove TypedChoiceField extra args
+        supercls_init_args = getargspec(supercls.__init__).args[1:]
+        supercls.__init__(
+            *args,
+            **{key: val for (key, val) in kwargs.items()
+               if key in supercls_init_args}
+        )
 
 class MultiSelectField(models.Field):
 
     def get_internal_type(self):
         return "CharField"
 
-    def get_choices_default(self):
-        return self.get_choices(include_blank=False)
-
-    def _get_FIELD_display(self, field):
-        value = getattr(self, field.attname)
-        choicedict = dict(field.choices)
-
     def formfield(self, **kwargs):
-        # don't call super, as that overrides default widget if it has choices
         defaults = {
-            'required': not self.blank,
-            'label': capfirst(self.verbose_name),
-            'help_text': self.help_text,
-            'choices': self.choices
+            'choices_form_class': MultiSelectFormField
         }
-        if self.has_default():
-            defaults['initial'] = self.get_default()
         defaults.update(kwargs)
-        return MultiSelectFormField(**defaults)
+        return super(MultiSelectField, self).formfield(**defaults)
 
     def get_prep_value(self, value):
-        return value
-
-    def get_db_prep_value(self, value, connection=None, prepared=False):
         if isinstance(value, six.string_types):
             return value
-        elif isinstance(value, list):
+        if isinstance(value, list):
             return ",".join(value)
+        return ''
 
     def from_db_value(self, value, expression, connection, context):
         return value.split(',') if value is not None else []
 
     def to_python(self, value):
-        if value is not None:
-            return value if isinstance(value, list) else value.split(',')
-        return ''
+        if isinstance(value, six.string_types):
+            return value.split(',') if value else []
+        if isinstance(value, list):
+            return value
+        return []
 
-    def contribute_to_class(self, cls, name):
-        super(MultiSelectField, self).contribute_to_class(cls, name)
+    def contribute_to_class(self, cls, name, *args, **kwargs):
+        super(MultiSelectField, self).contribute_to_class(
+            cls, name, *args, **kwargs)
         if self.choices:
-            func = lambda self, fieldname = name, choicedict = dict(self.choices): ",".join([choicedict.get(value, value) for value in getattr(self, fieldname)])
-            setattr(cls, 'get_%s_display' % self.name, func)
+            def _get_FIELD_display(self, field):
+                values = getattr(self, field.attname)
+                choices = dict(field.flatchoices)
+                return ",".join([
+                    force_text(
+                        choices.get(value, value),
+                        strings_only=True
+                    )
+                    for value in values
+                ])
+            setattr(cls, 'get_%s_display' % self.name,
+                    curry(_get_FIELD_display, field=self))
 
     def validate(self, value, model_instance):
-        arr_choices = self.get_choices_selected(self.get_choices_default())
-        for opt_select in value:
-            if (opt_select not in arr_choices):  # the int() here is for comparing with integer choices
-                raise exceptions.ValidationError(self.error_messages['invalid_choice'] % value)
+        # assume value should be a list
+        if not isinstance(value, list):
+            raise ValidationError(
+                self.error_messages['invalid'],
+                code='invalid',
+                params={'value': value},
+            )
+        if not value and not self.null:
+            raise ValidationError(
+                self.error_messages['null'],
+                code='null'
+            )
+        for option in value:
+            super(MultiSelectField, self).validate(
+                option, model_instance
+            )
         return
-
-    def get_choices_selected(self, arr_choices=''):
-        if not arr_choices:
-            return False
-        list = []
-        for choice_selected in arr_choices:
-            list.append(choice_selected[0])
-        return list
-
-    def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
-        return self.get_db_prep_value(value)
 
 # https://www.djangosnippets.org/snippets/2402/
 def get_namedtuple_choices(*choices_tuples):
