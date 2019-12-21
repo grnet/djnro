accab35816fad1244571109908c6ae2175e7a433
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2019-12-21 09:06:13 +0200

    Simplify edumanage.templatetags.tolocale with simple_tag()
    And add variant tolocale_get with assignment_tag()

--- edumanage/templatetags/tolocale.py
+++ edumanage/templatetags/tolocale.py
@@ -2,27 +2,8 @@ from django import template
 
 register = template.Library()
 
-@register.tag
-def tolocale(parser, token):
-    try:
-        tag_name, objtrans, format_string = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError("%r tag requires exactly two arguments" % token.contents.split()[0])
-    return CurrentLocaleNode(objtrans, format_string)
-
-
-class CurrentLocaleNode(template.Node):
-    def __init__(self, objtrans, format_string):
-        self.format_string = template.Variable(format_string)
-        self.objtrans = template.Variable(objtrans)
-    def render(self, context):
-        objtrans = self.objtrans.resolve(context)
-        translang = self.format_string.resolve(context)
-        return tolocale_filter(objtrans, translang)
-
-
-@register.filter(name='tolocale')
-def tolocale_filter(objtrans, translang):
+@register.filter
+def tolocale(objtrans, translang):
     try:
         return objtrans.get_name(lang=translang)
     except AttributeError:
@@ -30,3 +11,6 @@ def tolocale_filter(objtrans, translang):
             return objtrans.get(translang, '')
         else:
             return objtrans
+
+register.simple_tag(tolocale)
+register.assignment_tag(tolocale, name='tolocale_get')
