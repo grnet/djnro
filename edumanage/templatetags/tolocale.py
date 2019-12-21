a53c789aa08b319ec0d0d26fe52e0385d7592352
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2019-12-21 09:09:37 +0200

    Let tolocale accept an optional parameter to determine the method to call

--- edumanage/templatetags/tolocale.py
+++ edumanage/templatetags/tolocale.py
@@ -3,9 +3,10 @@ from django import template
 register = template.Library()
 
 @register.filter
-def tolocale(objtrans, translang):
+def tolocale(objtrans, translang, get='name'):
     try:
-        return objtrans.get_name(lang=translang)
+        objtrans_method = getattr(objtrans, 'get_{}'.format(get))
+        return objtrans_method(lang=translang)
     except AttributeError:
         if isinstance(objtrans, dict):
             return objtrans.get(translang, '')
